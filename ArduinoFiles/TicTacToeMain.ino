6329effa44928b9c34b653bc05686c6143aec66e
RibeiroSilvaFilipe <57909649+RibeiroSilvaFilipe@users.noreply.github.com>
2019-12-08 13:47:40 +0100

    Main program && arbeitsjournal (#13)
    * Update README.md
    
    Add spaces
    
    * Create ArduinoMainProgram.ino
    
    Add main class for arduino
    
    * Create Konzept_Grobaufbau.docx
    
    * add files
    
    - Add Taetigkeitsliste.docx
    - Add Flussdiagramm
    
    * Add no-win case to pap
    
    * add concept for program
    
    * as (#9)
    
    * Bipo feature (#2)
    
    * Update README.md
    
    * Update README.md
    
    Add spaces
    
    * Bipo feature (#3)
    
    * Update README.md
    
    * Update README.md
    
    Add spaces
    
    * Create ArduinoMainProgram.ino
    
    Add main class for arduino
    
    * Bipo feature (#4)
    
    * Update README.md
    
    * Update README.md
    
    Add spaces
    
    * Create ArduinoMainProgram.ino
    
    Add main class for arduino
    
    * Create Konzept_Grobaufbau.docx
    
    * Bipo feature (#5)
    
    * Update README.md
    
    * Update README.md
    
    Add spaces
    
    * Create ArduinoMainProgram.ino
    
    Add main class for arduino
    
    * Create Konzept_Grobaufbau.docx
    
    * add files
    
    - Add Taetigkeitsliste.docx
    - Add Flussdiagramm
    
    * Samir feature (#6)
    
    * Update README.md
    
    * Create Q&A.docx
    
    * Update Q&A.docx
    
    Erste Schritte
    
    * Skizzen
    
    Hinzufügung von Skizzen
    
    * delete
    
    * Delete TicTacToe.png
    
    * Delete M242_Flussdiagramm.pap
    
    * Bipo feature (#7)
    
    * Update README.md
    
    * Update README.md
    
    Add spaces
    
    * Create ArduinoMainProgram.ino
    
    Add main class for arduino
    
    * Create Konzept_Grobaufbau.docx
    
    * add files
    
    - Add Taetigkeitsliste.docx
    - Add Flussdiagramm
    
    * Add no-win case to pap
    
    * add concept for program
    
    * Samir feature (#8)
    
    * Update README.md
    
    * Create Q&A.docx
    
    * Update Q&A.docx
    
    Erste Schritte
    
    * Skizzen
    
    Hinzufügung von Skizzen
    
    * delete
    
    * Create Zustandsdiagramm_Gesamt-Uebersicht.PNG
    
    * Journal & move files
    
    * LedBlink
    
    * dadada
    
    * led blink && interrupt
    
    * SwitchLed_V1

--- ArduinoFiles/TicTacToeMain.ino
+++ ArduinoFiles/TicTacToeMain.ino
@@ -1,4 +1,28 @@
 // Array begins top right to bottom left
+
+// 0 = off
+// 1 = green
+// 2 = red
+// 3 = blink
+int ArrayForActiveLEDs[9] = {0, 0, 0, 0, 0, 0, 0, 0, 0};
+int CurrentLED = 8;
+int pinButtonLedSwitch = 2;
+int pinButtonLedConfirm = 3;
+
+int LatchPinRED = 8;
+int ClockPinRED = 13;
+int DataPinRED = 11;
+
+int LatchPinGREEN = 7;
+int ClockPinGREEN = 12;
+int DataPinGREEN = 10;
+
+bool isGreen = true;
+int byteOnGreen;
+byte byteOffGreen;
+byte byteOnRed;
+byte byteOffRed;
+=======
 int ArrayForActiveLEDs[9] = {1, 0, 0, 0, 0, 0, 0, 0, 0};
 
 int latchPinRED = 8;
@@ -11,10 +35,28 @@ int dataPinGREEN = 10;
 
 bool isGreen = true;
 
+
 void setup()
 {
     Serial.begin(9600);
     // shift red
+
+    pinMode(LatchPinGREEN, OUTPUT);
+    pinMode(DataPinRED, OUTPUT);
+    pinMode(ClockPinRED, OUTPUT);
+    // shift green
+    pinMode(LatchPinGREEN, OUTPUT);
+    pinMode(DataPinGREEN, OUTPUT);
+    pinMode(ClockPinGREEN, OUTPUT);
+    // single leds
+    pinMode(5, OUTPUT); // red OR
+    pinMode(6, OUTPUT); // green OR
+    // buttons, 2 = next, 3 = confirm
+    pinMode(2, INPUT_PULLUP);
+    pinMode(3, INPUT_PULLUP);
+    attachInterrupt(digitalPinToInterrupt(2), ButtonLedSwitch, FALLING);
+    attachInterrupt(digitalPinToInterrupt(3), ButtonLedConfirm, FALLING);
+=======
     pinMode(latchPinRED, OUTPUT);
     pinMode(dataPinRED, OUTPUT);
     pinMode(clockPinRED, OUTPUT);
@@ -28,10 +70,163 @@ void setup()
     // buttons, 3 = next, 4 = confirm
     attachInterrupt(0, ButtonLedSwitch, CHANGE);
     attachInterrupt(1, ButtonLedConfirm, CHANGE);
+
 }
 
 void loop()
 {
+
+    Blink();
+}
+
+void Blink()
+{
+    if (isGreen)
+    {
+        digitalWrite(LatchPinGREEN, LOW);
+        shiftOut(DataPinGREEN, ClockPinGREEN, byteOffGreen);
+        digitalWrite(LatchPinGREEN, HIGH);
+        digitalWrite(6, 0);
+        delay(1000);
+        digitalWrite(LatchPinGREEN, LOW);
+        shiftOut(DataPinGREEN, ClockPinGREEN, byteOnGreen);
+        digitalWrite(LatchPinGREEN, HIGH);
+        digitalWrite(6, ArrayForActiveLEDs[0]);
+        delay(1000);
+    }
+    else
+    {
+        digitalWrite(LatchPinRED, LOW);
+        shiftOut(DataPinRED, ClockPinRED, byteOffRed);
+        digitalWrite(LatchPinRED, HIGH);
+        digitalWrite(5, 0);
+        delay(1000);
+        digitalWrite(LatchPinRED, LOW);
+        shiftOut(DataPinRED, ClockPinRED, byteOnRed);
+        digitalWrite(LatchPinRED, HIGH);
+        digitalWrite(5, ArrayForActiveLEDs[0]);
+        delay(1000);
+    }
+}
+void ButtonLedSwitch()
+{
+    int nextLED = FindNextLED();
+    ArrayForActiveLEDs[nextLED] = 3;
+
+    if (nextLED == 9)
+    {
+        // blink all
+    }
+    ParseArrayToString(isGreen);
+}
+
+int FindNextLED()
+{
+    int led = CurrentLED;
+    for (int i = 0; i < 9; i++)
+    {
+        if (ArrayForActiveLEDs[led] != 0)
+        {
+            if (led == 0)
+            {
+                led = 8;
+            }
+            else
+            {
+                led--;
+            }
+        }
+        Serial.println(led);
+    }
+    ArrayForActiveLEDs[CurrentLED] = 0;
+    CurrentLED = led;
+    return led;
+}
+
+void ButtonLedConfirm()
+{
+    bool win = false;
+
+    if (isGreen)
+    {
+        isGreen = false;
+        win = (CheckForWin(ArrayForActiveLEDs, 1));
+    }
+    else
+    {
+        isGreen = true;
+        win = (CheckForWin(ArrayForActiveLEDs, 2));
+    }
+    Serial.println("Confirmed");
+}
+
+void ParseArrayToString(bool isGreen)
+{
+    String byteStringOnGreen = "";
+    String byteStringOffGreen = "";
+    String byteStringOnRed = "";
+    String byteStringOffRed = "";
+    if (isGreen)
+    {
+        for (int i = 0; i < 9; i++)
+        {
+            if (ArrayForActiveLEDs[i] == 0)
+            {
+                byteStringOnGreen += '0';
+                byteStringOffGreen += '0';
+            }
+            if (ArrayForActiveLEDs[i] == 1)
+            {
+                byteStringOnGreen += '1';
+                byteStringOffGreen += '1';
+            }
+            if (ArrayForActiveLEDs[i] == 2)
+            {
+                byteStringOnGreen += '0';
+                byteStringOffGreen += '0';
+            }
+            if (ArrayForActiveLEDs[i] == 3)
+            {
+                byteStringOnGreen += '1';
+                byteStringOffGreen += '0';
+            }
+        }
+    }
+    if (!isGreen)
+    {
+        for (int i = 0; i < 9; i++)
+        {
+            if (ArrayForActiveLEDs[i] == 0)
+            {
+                byteStringOnRed += '0';
+                byteStringOffRed += '0';
+            }
+            if (ArrayForActiveLEDs[i] == 1)
+            {
+                byteStringOnRed += '0';
+                byteStringOffRed += '0';
+            }
+            if (ArrayForActiveLEDs[i] == 2)
+            {
+                byteStringOnRed += '1';
+                byteStringOffRed += '1';
+            }
+            if (ArrayForActiveLEDs[i] == 3)
+            {
+                byteStringOnRed += '1';
+                byteStringOffRed += '0';
+            }
+        }
+    }
+int a = atoi( byteStringOffGreen.c_str() );
+
+    byteOffGreen = a;
+    Serial.println(byteOffGreen + 'g');
+    /*byteOffGreen = PrintBitsSimple(byteStringOffGreen);
+    byteOnGreen = PrintBitsSimple(byteStringOnGreen);
+    byteOffRed = PrintBitsSimple(byteStringOffRed);
+    byteOnRed = PrintBitsSimple(byteStringOnRed);*/
+=======
     if (isGreen)
     {
         digitalWrite(latchPinGREEN, LOW);
@@ -69,6 +264,7 @@ void ButtonLedConfirm(){
     {
         // allahu akbar
     }
+
 }
 
 // the heart of the program
@@ -122,6 +318,41 @@ void shiftOut(int myDataPin, int myClockPin, byte myDataOut)
     digitalWrite(myClockPin, 0);
 }
 
+
+bool CheckForWin(int array[9], int color)
+{
+    if (array[8] == color && array[7] == color && array[6] == color)
+    {
+        return true; // UUU
+    }
+    else if (array[2] == color && array[5] == color && array[8] == color)
+    {
+        return true; // LLL
+    }
+    else if (array[0] == color && array[1] == color && array[2] == color)
+    {
+        return true; // OOO
+    }
+    else if (array[0] == color && array[3] == color && array[6] == color)
+    {
+        return true; // RRR
+    }
+    else if (array[1] == color && array[4] == color && array[7] == color)
+    {
+        return true; // MMM ver
+    }
+    else if (array[3] == color && array[4] == color && array[5] == color)
+    {
+        return true; // MMM hor
+    }
+    else if (array[0] == color && array[4] == color && array[8] == color)
+    {
+        return true; // OR-UL diagonal
+    }
+    else if (array[2] == color && array[4] == color && array[6] == color)
+    {
+        return true; // OL-UR diagonal
+=======
 bool CheckForWin(int array[9])
 {
     switch (1)
@@ -147,5 +378,6 @@ bool CheckForWin(int array[9])
     case array{0,0,2,0,2,0,2,0,0}: return true; // OL-UR diagonal*/
     default:
         return false;
+
     }
 }
